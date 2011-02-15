9edf6a7c975cc07d3cad734baba036dcd2fc2097
mvliet <matt.vliet@gmail.com>
2011-02-15 12:16:28 -0800

    remove the make-config subcommand.  configuration will now be created automatically if not found.

--- repoman-client/repoman_client/config.py
+++ repoman-client/repoman_client/config.py
@@ -98,7 +98,8 @@ class Config(object):
         self.verbose = verbose
         if not self.config_file:
             print "No configuration file found."
-            sys.exit(1)
+            self.generate_config()
+            print "Generating new confi file at '%s'" % self.config_file
         self._validate_options()
         self._check_logging()
         self._check_proxy()
