4d7087f5fff9901c4674474f5de03177c0e8586a
mvliet <matt.vliet@gmail.com>
2011-03-09 10:43:55 -0800

    turn content-length middleware on for responses

--- server/repoman/repoman/config/middleware.py
+++ server/repoman/repoman/config/middleware.py
@@ -13,6 +13,7 @@ from repoman.config.environment import load_environment
 from repoman.lib.middleware.authentication import UserAuthentication
 from repoman.lib.middleware.filestorage import StorageMiddleware
 from repoman.lib.middleware.cascade import BypassCascade
+from repoman.lib.middleware.content_length import ContentLengthMiddleware
 
 def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
     """Create a Pylons WSGI application and return it
@@ -77,6 +78,8 @@ def make_app(global_conf, full_stack=True, static_files=True, **app_conf):
 
     app = StorageMiddleware(app, temp=config['global_conf'].get('temp_storage'),
                             hash_type=config['global_conf'].get('hash_type'))
+
+    app = ContentLengthMiddleware(app)
 
     app.config = config
     return app
