c212f160fdcf5a6859d77ddf835c3fc6f7b7d4ec
Tubego Phamphang <tphampha@heplw29.phys.UVic.CA>
2011-11-17 14:09:13 -0800

    Fix for the issue:"failing to provide image name on modify-image causes nasty error message"

--- repoman-client/repoman_client/subcommands/modify.py
+++ repoman-client/repoman_client/subcommands/modify.py
@@ -1,7 +1,7 @@
 from repoman_client.subcommand import SubCommand
 from repoman_client.client import RepomanClient, RepomanError
 from repoman_client.config import config
-from repoman_client.parsers import parse_unknown_args, ArgumentFormatError
+from repoman_client.parsers import parse_unknown_args, ArgumentFormatError, arg_value_pairs
 from argparse import ArgumentParser
 import sys
 import logging
@@ -93,9 +93,17 @@ class ModifyImage(SubCommand):
 
     def get_parser(self):
         p = ArgumentParser(self.description)
-        p.usage = "modify-image [-h] image [--metadata value [--metadata value ...]]"
-        p.epilog = "See documentation for a list of required and optional metadata"
+        p.usage = "modify-image image [-h] [--name] [--description] [--os_variant] [--os_arch] [--os_type] [--hypervisor] [--read_only] [--expires] [--unauthenticated_access]"
         p.add_argument('image', help='The existing image you want to modify')
+        p.add_argument('--name', help='image name unique to the users namespace')
+        p.add_argument('--description', help='description of the image modification')
+        p.add_argument('--os_variant', help='redhat, centos, ubuntu, etc')
+        p.add_argument('--os_arch', help='x86, x86_64')
+        p.add_argument('--os_type', help='linux, unix, windows, etc')
+        p.add_argument('--hypervisor', help='kvm, xen')
+        p.add_argument('--read_only', default=False, help='should the image be read only?')
+        p.add_argument('--expires', help='when should the image expire?')
+        p.add_argument('--unauthenticated_access', default=True, help='should the image access be unauthenticated?')
         return p
 
     def __call__(self, args, extra_args=None):
@@ -103,22 +111,26 @@ class ModifyImage(SubCommand):
         log.debug("args: '%s' extra_args: '%s'" % (args, extra_args))
     
         repo = RepomanClient(config.host, config.port, config.proxy)
-        if extra_args:
-            try:
-                kwargs = parse_unknown_args(extra_args)
-            except ArgumentFormatError, e:
-                print e.message
-                sys.exit(1)
-        else:
-            kwargs={}
-            
+  
+        kwargs={}
+        extra_args=['name',args.name,'description',args.description,'os_variant',args.os_variant,'os_arch',args.os_arch,'os_type',args.os_type,
+                   'hypervisor',args.hypervisor,'read_only',args.read_only,'expires',args.expires,'unauthenticated_access',args.unauthenticated_access]
+        for arg,value in arg_value_pairs(extra_args):
+            if value:
+                if value in ['true', 'True', 'TRUE']:
+                    value = True
+                elif value in ['false', 'False', 'FALSE']:
+                    value = False
+                kwargs.update({arg:value})
+        del extra_args    
         log.debug("kwargs: '%s'" % kwargs)
 
         try:
             repo.modify_image(args.image, **kwargs)
             print "[OK]     Modifying image."
         except RepomanError, e:
-            print "[FAILED] Modifying image.\n\t-%s" % e
+            print "[FAILED] Modifying image.\n\t-The image you are trying to modify does not exist in the repository"
+            log.debug(e.message) 
             sys.exit(1)
 
 
