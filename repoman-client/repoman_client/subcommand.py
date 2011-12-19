20bb149b9daa33039e9b3be602a438d76ddb4a62
Andre Charbonneau <4433258@gmail.com>
2011-12-19 18:08:55 -0500

    example arg parsing code for make-config command (non OO approach)

--- repoman-client/repoman_client/subcommand.py
+++ repoman-client/repoman_client/subcommand.py
@@ -7,16 +7,11 @@ class SubCommand(object):
     """A baseclass that all subcommands must be subclassed from.
 
     required methods:
-        get_parser(self)
-            - The must return an argparse.ArgumentParser object prepopulated
-              with the command options.
 
-        __call__(self, args, extra_args=None)
+        __call__(self, args)
             - This is the entry point that will be called to execute the
               subcommand.
             - args - contains the parsed commandline args
-            - extra_args - contains unparsed commandline args the option is
-                           specified for the class.
             What you do from this point on is up to you.
 
     """
@@ -24,21 +19,9 @@ class SubCommand(object):
                                 # before the subcommand is run.
                                 # If the subcommand depends on configuration values
                                 # this should remain True.
-    hidden = False              # if True, this command will not appear in the help
-    command_group = None        # the name of a group the command will be added to
-    command = None              # the command
-    alias = None                # a short alias for the command
-    parse_known_args = False    # determined if unknown commandline arguments
-                                # will be allowed and passed to the __call__ method
-    description = ""            # a description string that will show up in a
-                                # list of subcommands
     require_sudo = False        # Will check to see that the user is sudo before
                                 # executing command.                                
 
-    def get_parser(self):
-        # Raise an exception to make sure people override this in the subclass
-        raise Exception("You need to override the 'get_parser' class method")
-
-    def __call__(self, args, extra_args=None):
+    def __call__(self, args):
         # Raise an exception to make sure people override this in the subclass
         raise Exception("You need to override the '__call__' class method")
