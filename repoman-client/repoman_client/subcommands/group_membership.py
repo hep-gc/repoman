300bc6771a3bc6a127ac46afa6125017911700da
Andre Charbonneau <4433258@gmail.com>
2012-01-11 11:56:43 -0500

    add-users-to-group done

--- repoman-client/repoman_client/subcommands/group_membership.py
+++ repoman-client/repoman_client/subcommands/group_membership.py
@@ -6,21 +6,19 @@ import logging
 
 
 class AddUserToGroup(SubCommand):
-    command_group = 'advanced'
     command = 'add-users-to-group'
-    alias = 'autg'
-    description = 'Add make specifed users members of a group'
+    alias = 'aug'
+    description = 'Add specifed users to a group.'
 
-    def get_parser(self):
-        p = ArgumentParser(self.description)
-        p.add_argument('group', help='group to add users to')
-        p.add_argument('-u', '--users', nargs='+', metavar='USER', help='users to add')
-        return p
+    def __init__(self):
+        SubCommand.__init__(self)
 
-    def __call__(self, args, extra_args=None):
-        log = logging.getLogger('AddUserToGroup')
-        log.debug("args: '%s' extra_args: '%s'" % (args, extra_args))
-        
+    def init_arg_parser(self):
+        self.get_arg_parser().add_argument('group', help = 'The name of the newly created group.  It must be unique and can only contain ([a-Z][0-9][_][-]) characters.')
+        self.get_arg_parser().add_argument('users', metavar = 'user', nargs = '+', help = 'The user(s) to add to the group.')
+        self.get_arg_parser().set_defaults(func=self)
+
+    def __call__(self, args):
         repo = RepomanClient(config.host, config.port, config.proxy)
         for user in args.users:
             status = "Adding user: `%s` to group: '%s'\t\t" % (user, args.group)
