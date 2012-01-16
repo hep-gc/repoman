6e1657ec21527180c1a80ae8e909a970530440e6
Andre Charbonneau <4433258@gmail.com>
2012-01-16 15:35:25 -0500

    unshare-image-with-users and unshare-image-with-groups done

--- repoman-client/repoman_client/subcommands/sharing.py
+++ repoman-client/repoman_client/subcommands/sharing.py
@@ -34,6 +34,7 @@ class ShareImageWithGroups(SubCommand):
                 print "[FAILED] Shared image: '%s' with group: '%s'\n\t-%s" % (args.image, group, e)
                 sys.exit(1)
 
+
 class ShareImageWithUsers(SubCommand):
     command = 'share-image-with-users'
     alias = 'siu'
@@ -65,44 +66,64 @@ class ShareImageWithUsers(SubCommand):
 
 
 
-class UnshareImage(SubCommand):
-    command_group = 'advanced'
-    command = 'unshare-image'
-    alias = 'ui'
-    description = 'Remove a share from an image'
+class UnshareImageWithGroups(SubCommand):
+    command = 'unshare-image-with-groups'
+    alias = 'uig'
+    description = 'Unshare an image with one or more groups.'
 
-    def get_parser(self):
-        p = ArgumentParser(self.description)
-        p.add_argument('image', help='Image to unshare')
-        g = p.add_mutually_exclusive_group()
-        g.add_argument('-u', '--user', help='User to remove share from')
-        g.add_argument('-g', '--group', help='Group to remove share from')
-        #g.add_argument('-a', '--all', help='Remove all shares')
-        return p
+    def __init__(self):
+        SubCommand.__init__(self)
 
-    def __call__(self, args, extra_args=None):
-        log = logging.getLogger('UnshareImage')
-        log.debug("args: '%s' extra_args: '%s'" % (args, extra_args))
-    
+    def init_arg_parser(self):
+        self.get_arg_parser().add_argument('image', help='The image to unshare. Use "repoman list-images" to see possible values.')
+        self.get_arg_parser().add_argument('group', nargs = '+', help='The name of the group(s) to unshare the image with. Use "repoman describe-image" to see possible values.')
+        self.get_arg_parser().add_argument('-o', '--owner', metavar = 'user', help = 'The owner of the named image. The default is the ID of the current repoman user which can be determined with the command "repoman whoami" command.')
+        self.get_arg_parser().set_defaults(func=self)
+
+    def __call__(self, args):
         repo = RepomanClient(config.host, config.port, config.proxy)
-        status = "Unshared image: '%s' with: '%s'"
-        if args.user:
-            func = repo.unshare_with_user
-            kwargs = {'user':args.user}
-            status = status % (args.image, args.user)
-        elif args.group:
-            func = repo.unshare_with_group
-            kwargs = {'group':args.group}
-            status = status % (args.image, args.group)
-        else:
-            kwargs = {}
-        
-        log.debug("kwargs: '%s'" % kwargs)
+        for group in args.group:
+            try:
+                kwargs = {'group':group}
+                if args.owner:
+                    repo.unshare_with_group(args.owner + '/' + args.image, **kwargs)
+                else:
+                    repo.unshare_with_group(args.image, **kwargs)
 
-        try:
-            func(args.image, **kwargs)
-            print "[OK]     %s" % status
-        except RepomanError, e:
-            print "[FAILED] %s\n\t-%s" % (status, e)
-            sys.exit(1)
+                print "[OK]     Unshared image: '%s' with group: '%s'" % (args.image, group)
+            except RepomanError, e:
+                print "[FAILED] Unsharing image: '%s' with group: '%s'\n\t-%s" % (args.image, group, e)
+                sys.exit(1)
+
+
+class UnshareImageWithUsers(SubCommand):
+    command = 'unshare-image-with-users'
+    alias = 'uiu'
+    description = 'Unshare an image with one or more users.'
+
+    def __init__(self):
+        SubCommand.__init__(self)
+
+    def init_arg_parser(self):
+        self.get_arg_parser().add_argument('image', help='The image to unshare. Use "repoman list-images" to see possible values.')
+        self.get_arg_parser().add_argument('user', nargs = '+', help='The name of the user(s) to unshare the image with. Use "repoman describe-image" to see possible values.')
+        self.get_arg_parser().add_argument('-o', '--owner', metavar = 'user', help = 'The owner of the named image. The default is the ID of the current repoman user which can be determined with the command "repoman whoami" command.')
+        self.get_arg_parser().set_defaults(func=self)
+
+    def __call__(self, args):
+        repo = RepomanClient(config.host, config.port, config.proxy)
+        for user in args.user:
+            try:
+                kwargs = {'user':user}
+                if args.owner:
+                    repo.unshare_with_user(args.owner + '/' + args.image, **kwargs)
+                else:
+                    repo.unshare_with_user(args.image, **kwargs)
+
+                print "[OK]     Unshared image: '%s' with user: '%s'" % (args.image, user)
+            except RepomanError, e:
+                print "[FAILED] Unsharing image: '%s' with user: '%s'\n\t-%s" % (args.image, user, e)
+                sys.exit(1)
+
+
 
