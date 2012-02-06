453cb1ee137ca03c6cddb1491e4a80f205ea0fcd
Andre Charbonneau <4433258@gmail.com>
2012-02-06 17:24:19 -0500

    removed -e and -E from make-config

--- repoman-client/repoman_client/subcommands/config.py
+++ repoman-client/repoman_client/subcommands/config.py
@@ -12,10 +12,6 @@ class MakeConfig(SubCommand):
         SubCommand.__init__(self)
 
     def init_arg_parser(self):
-        self.get_arg_parser().add_argument('-E', '--system_excludes', metavar='system_paths',
-                                   help = 'Blank separated list of paths to be excluded from a snapshot of the operating system during a "repoman save-image".  A directory path specification ending in "/*" will cause the directory to be created in the saved image, but none of it\'s contents to be copied to the saved image.  Defaults to "/cvmfs/* /dev/* /mnt/* /proc/* /root/.ssh /sys/* /tmp/*".')
-        self.get_arg_parser().add_argument('-e', '--user-excludes', metavar='user_paths',
-                                   help = 'Blank separated list of paths to be excluded from a snapshot of the operating system during a "repoman save-image".  A directory path specification ending in "/*" will cause the directory to be created in the saved image, but none of it\'s contents to be copied to the saved image.  Defaults to an empty list.')
         self.get_arg_parser().add_argument('--stdout', action='store_true', help = 'Send the configuration to stdout instead of writing it to a file under the current user\'s home directory.')
 
 
