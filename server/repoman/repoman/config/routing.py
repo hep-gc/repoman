c417b31eaced02217fd15f09ffe4187b6bebea41
Andre Charbonneau <4433258@gmail.com>
2012-02-14 16:26:17 -0500

    checkpoint commit

--- server/repoman/repoman/config/routing.py
+++ server/repoman/repoman/config/routing.py
@@ -5,6 +5,7 @@ may take precedent over the more generic routes. For more information
 refer to the routes manual at http://routes.groovie.org/docs/
 """
 from routes import Mapper
+import logging
 
 log = logging.getLogger(__name__)
 
