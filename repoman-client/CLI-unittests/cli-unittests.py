5f50e71ffaf5f22d4310c2522cd3e7bf7b77b5b1
Andre Charbonneau <4433258@gmail.com>
2012-02-22 11:45:08 -0500

    initial checking of simply CLI unit testing

---
+++ repoman-client/CLI-unittests/cli-unittests.py
@@ -0,0 +1,110 @@
+import re
+import sys
+import uuid
+import unittest
+from subprocess import Popen, PIPE, STDOUT
+
+
+class RepomanCLITest(unittest.TestCase):
+    """
+    Base class for all Repoman CLI tests.
+    """
+
+    def setUp(self):
+        # Nothing to do for now...
+        pass
+
+    def tearDown(self):
+        # Nothing to do for now...
+        pass
+
+    def run_repoman_command(self, args):
+        """
+        This method will call the repoman application in a subprocess
+        and pass it the given command line argument string.
+        The output and the return code of the repoman invocation will be
+        returned to the caller for further inspection.
+        """
+        try:
+            cmd = 'repoman %s' % (args)
+            print 'Running [%s]' % (cmd)
+            p = Popen(cmd, shell=True, stdout=PIPE, stderr=STDOUT)
+            output = p.communicate()[0]
+            return (output, p.returncode)
+        except Exception, e:
+            print 'Error running repoman command.\n%s' % (e)
+            sys.exit(-1)
+            
+
+    def get_unique_image_name(self):
+        """
+        Use this method to easily create a unique image name to use in 
+        your tests.
+        """
+        return str(uuid.uuid1())
+
+
+
+
+#####################################################################
+#####################################################################
+#####################################################################
+#
+# Test cases implementation go here...
+#
+#
+
+class ClientVersionTest(RepomanCLITest):
+    def runTest(self):
+        (output, returncode) = self.run_repoman_command('version')
+        self.assertEqual(output, '0.3.11\n')
+        self.assertEqual(returncode, 0)
+
+class WhoamiTest(RepomanCLITest):
+    def runTest(self):
+        (output, returncode) = self.run_repoman_command('whoami')
+        self.assertEqual(returncode, 0)
+
+
+class CreateImageTest(RepomanCLITest):
+    new_image_name = None
+    def runTest(self):
+        # Get a unique name for the new image we are going to create.
+        # This will ensure it does not clash with any existing image on the
+        # server.
+        self.new_image_name = self.get_unique_image_name()
+
+        # Now let's run the 'repoman create-image' command and check it's
+        # output and exit code.
+        (output, returncode) = self.run_repoman_command('create-image %s' % (self.new_image_name))
+        self.assertEqual(output, "[OK]     Created new image '%s'\n" % (self.new_image_name))
+        self.assertEqual(returncode, 0)
+
+        # Here we actually do a 'repoman list-image' to see if the image is
+        # actually there.  We use a regular expression to inspect the output.
+        (output, returncode) = self.run_repoman_command('list-images %s' % (self.new_image_name))
+        m = re.search('^\\s+name : %s\\s*$' % (self.new_image_name), 
+                      output, 
+                      flags=re.MULTILINE)
+        self.assertTrue(m != None)
+            
+    def tearDown(self):
+        # Delete the image we created to clean up properly after the test.
+        (output, returncode) = self.run_repoman_command('remove-image -f %s' % (self.new_image_name))
+
+
+#####################################################################
+#####################################################################
+#####################################################################
+
+
+
+
+#
+# The entrypoint into the script.
+# No need to modify this at this point.
+#
+
+if __name__ == '__main__':
+    unittest.main()
+
