ed51d39650f3eb46f37f1add0a47dbea6b681fc4
Pranav <pranav@uvic.ca>
2012-02-28 12:53:34 -0800

    Unit test for some commands - version, whoami, create-image (and some optional parameters)

--- repoman-client/CLI-unittests/cli-unittests.py
+++ repoman-client/CLI-unittests/cli-unittests.py
@@ -54,29 +54,43 @@ class RepomanCLITest(unittest.TestCase):
 #
 #
 
+######################################################################
+#		COMMAND - 'repoman version'
+######################################################################
 class ClientVersionTest(RepomanCLITest):
-    def runTest(self):
+    def test_version(self):
         (output, returncode) = self.run_repoman_command('version')
         self.assertEqual(output, '0.3.11\n')
         self.assertEqual(returncode, 0)
 
+######################################################################
+#		COMMAND - 'repoman whoami'
+######################################################################
+
 class WhoamiTest(RepomanCLITest):
-    def runTest(self):
+    def test_whoami(self):
         (output, returncode) = self.run_repoman_command('whoami')
         self.assertEqual(returncode, 0)
 
+######################################################################
+#		COMMAND - 'repoman create-image'		     	
+######################################################################
 
 class CreateImageTest(RepomanCLITest):
     new_image_name = None
-    def runTest(self):
+	
+	# CreateImage function is called whenever the create-image command is used (with or without optional parameters)
+	# Optional parameters are stored in arg. 
+	# When create image is called without options, arg is passed as an empty string
+    def CreateImage(self, arg):
         # Get a unique name for the new image we are going to create.
         # This will ensure it does not clash with any existing image on the
         # server.
         self.new_image_name = self.get_unique_image_name()
-
+	
         # Now let's run the 'repoman create-image' command and check it's
         # output and exit code.
-        (output, returncode) = self.run_repoman_command('create-image %s' % (self.new_image_name))
+        (output, returncode) = self.run_repoman_command('create-image %s %s' % (self.new_image_name, arg))
         self.assertEqual(output, "[OK]     Created new image '%s'\n" % (self.new_image_name))
         self.assertEqual(returncode, 0)
 
@@ -86,12 +100,73 @@ class CreateImageTest(RepomanCLITest):
         m = re.search('^\\s+name : %s\\s*$' % (self.new_image_name), 
                       output, 
                       flags=re.MULTILINE)
-        self.assertTrue(m != None)
-            
+        self.assertTrue(m != None)        
+	
+ 			
+	# If unauthenticated access is set as true or false while creating an image,
+	# the status is checked in the output of 'repoman list-image' using regular expression 	
+	if(arg == "--unauthenticated_access true" or arg == "-a true"):
+		p = re.search('^\\s*unauthenticated_access : True\\s*$', output,flags=re.MULTILINE)
+        	self.assertTrue(p != None)
+       
+	elif (arg == '--unauthenticated_access false' or arg == '-a false'):
+                p = re.search('^\\s*unauthenticated_access : False\\s*$',
+                      output,
+                      flags=re.MULTILINE)
+                self.assertTrue(p != None)
+	
+	elif (arg == '--description "Random description"' or arg == '--d "Random description"'):
+		p = re.search('^\\s*description : Random description\\s*$', output, flags=re.MULTILINE)
+		self.assertTrue(p != None)
+
     def tearDown(self):
         # Delete the image we created to clean up properly after the test.
-        (output, returncode) = self.run_repoman_command('remove-image -f %s' % (self.new_image_name))
+       	(output, returncode) = self.run_repoman_command('remove-image -f %s' % (self.new_image_name))
 
+   
+    def test_create_image(self):
+	CreateImageTest.CreateImage(self,'')
+
+    
+    # Test the alias of create-image command ('repoman ci')	
+    def test_ci(self):	
+	self.new_image_name = self.get_unique_image_name()
+	(output, returncode) = self.run_repoman_command('ci %s' % (self.new_image_name))
+        self.assertEqual(output, "[OK]     Created new image '%s'\n" % (self.new_image_name))
+        self.assertEqual(returncode, 0)
+	(output, returncode) = self.run_repoman_command('list-images %s' % (self.new_image_name))
+        m = re.search('^\\s+name : %s\\s*$' % (self.new_image_name),
+                      output,
+                      flags=re.MULTILINE)
+        self.assertTrue(m != None)
+
+
+	#########################################################
+	#		OPTIONAL PARAMETERS			#	
+	#########################################################
+
+    # Test the optional parameter '--unauthenticated_access' or '-a', which can be set to true or false
+    # Each case is in individual tests to ensure teardown (removing image after creating one)
+    def test_create_image_unauthenticated_access_true(self):
+	CreateImageTest.CreateImage(self,'--unauthenticated_access true')
+    def test_create_image_a_true(self):		
+	CreateImageTest.CreateImage(self,'-a true')
+    def test_create_image_unauthenticated_access_false(self):
+	CreateImageTest.CreateImage(self,'--unauthenticated_access false')       
+    def test_create_image_a_false(self):   
+	CreateImageTest.CreateImage(self,'-a false')
+
+
+    # Test the optional parameter '--description' or '-d' 
+    # The description given here is "Random description"
+    def test_create_image_description(self):
+	CreateImageTest.CreateImage(self, '--description "Random description"')
+    def test_create_image_d(self):
+        CreateImageTest.CreateImage(self, '--d "Random description"')
+ 
+	
+
+	
 
 #####################################################################
 #####################################################################
